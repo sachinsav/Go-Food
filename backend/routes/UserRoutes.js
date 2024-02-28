32a604aeec02854788bfb048726b6e8ea32709e9
sachinsav <sachin.saw.13@gmail.com>
2024-02-28 09:22:36 +0530

    login page integration done, password secured using bcrypt, jwt introduct to authorization

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const {body, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 route.post("/createuser", [
     body('name').isLength({min:3}),
@@ -9,11 +13,13 @@ route.post("/createuser", [
     body('password','password length is too short').isLength({min: 5})
 ], async (req, res) => {
     try{
+        const salt = await bcrypt.genSalt(10);
+        const securePwd = await bcrypt.hash(req.body.password, salt);
         const user = new User({
             name: req.body.name,
             location: req.body.location,
             email: req.body.email,
-            password: req.body.password
+            password: securePwd
         })
         const errors = validationResult(req);
         if(!errors.isEmpty()){
@@ -27,6 +33,25 @@ route.post("/createuser", [
         console.log(err);
     }
     
+})
+
+route.post("/login", async (req, res)=>{
+    const response = await User.findOne({email:req.body.email});
+    console.log(response)
+    if(!response){
+        console.log("email id does not present in db");
+        return res.status(400).json({errors: "invalid credentails"});
+    }
+    const passwordCompare = await bcrypt.compare(req.body.password, response.password);
+    if(!passwordCompare){
+        return res.status(400).json({errors: "invalid credentails"});
+    }
+    const userData = response.id;
+    const authToken = jwt.sign(userData, process.env.SECRET_KEY)
+    
+    return res.json({success: true, authToken: authToken});
+
+
 })
 
 module.exports = route
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+    const [credential, setCredentials] = useState({email:"", password:""});
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setCredentials({
+            ...credential,
+            [e.target.name] : e.target.value
+        })
+
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/login",{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credential)
+          });
+        const responseJson = await response.json();
+        if(!responseJson.success){
+            alert("Invalid Credential")
+            return;
+        }
+        console.log(responseJson)
+        localStorage.setItem("authToken",responseJson.authToken);
+        navigate("/");
+
+    }
   return (
-    <div>
-      
+    <div className="container mt-2 mb-3">
+      <form onSubmit={handleSubmit}>
+    
+        <div className="form-group">
+          <label htmlFor="userEmail">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="userEmail"
+            name="email"
+            onChange={handleChange}
+            value={credential.email}
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="userPwd">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="userPwd"
+            name="password"
+            onChange={handleChange}
+            value={credential.password}
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
     </div>
   )
 }
